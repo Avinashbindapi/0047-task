7d8a6c6d1e3ef3ed1fa4114cdcee3229f74cb023
Avinashbindapi <avinash.b@bindapi.com>
2022-09-20 15:16:13 +0530

    second commit

--- index.php
+++ index.php
@@ -12,18 +12,17 @@
         <input type="text" name="second_name" placeholder="SecondName">
         <button type="submit" name="send">send</button>
     </form>
-</body>
-</html>
-
+    
 <?php
-        
+    
     if (isset($_POST['send'])) 
     {
         $first_name = $_POST['first_name'];
 
         $second_name = $_POST['second_name'];
 
-        // echo($first_name),($second_name);
-
         echo  "<h2>Hello $first_name $second_name! Greetings from BindAPI.</h2>";
     }
+?>        
+</body>
+</html>
